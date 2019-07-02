313bbe00c52ea09077654aea5e6fa6c5c456aad4
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-07-02 13:11:28 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var playerOne = 1
+var playerTwo = 2
+
+if (playerOne > playerTwo) {
+	console.log("Player 1 has a higher number than Player 2.");
+} if else (playerTwo > playerOne) {
+	console.log("Player 2 has a higher number than Player 1.");
+} else ("You have guessed your opponent's number!");
