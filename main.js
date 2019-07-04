a7d12c40f10035e6e52904767a52fa3b072c830c
Jesse Maxim <jessmaxim303@gmail.com>
2019-07-04 13:26:38 -0600

    Add function of range finder

---
+++ main.js
@@ -0,0 +1,30 @@
+var numbers = [from 1, to 100];
+
+    // random value generated 
+    var y = Math.floor(Math.random() * 10 + 1); 
+      
+    // counting the number of guesses 
+    // made for correct Guess 
+    var guess = 1; 
+      
+    document.getElementById("submitguess").onclick = function(){ 
+      
+    // number guessed by user      
+    var x = document.getElementById("guessField").value; 
+  
+   if(x == y) 
+   {     
+       console.log(winningPlayer)
+               + guess + " GUESS "; 
+   } 
+   else if(x > y) /* if guessed number is greater 
+                   than actual number*/ 
+   {     
+       guess++; 
+       alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
+   } 
+   else
+   { 
+       guess++; 
+       alert("OOPS SORRY!! TRY A GREATER NUMBER") 
+   }  
