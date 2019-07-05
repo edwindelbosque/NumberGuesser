6425803a3389fe8dd82b80f3fe0c50960f1093ca
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-07-04 20:34:50 -0600

    Organized main.js

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+var numbers = [from 1, to 100];
+
+    // random value generated 
+    var y = Math.floor(Math.random() * 10 + 1); 
+      
+    // counting the number of guesses 
+    // made for correct Guess 
+    var guess = 1; 
+      
+    document.getElementById("submitguess").onclick = function(){ 
+      
+    // number guessed by user      
+    var x = document.getElementById("guessField").value; 
+  
+   if(x == y) 
+   {     
+       console.log(winningPlayer)
+               + guess + " GUESS "; 
+   } 
+   else if(x > y) /* if guessed number is greater 
+                   than actual number*/ 
+   {     
+       guess++; 
+       alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
+   } 
+   else
+   { 
+       guess++; 
+       alert("OOPS SORRY!! TRY A GREATER NUMBER")
+   } 
+};
+
+// This variables and function is to be able to change the challenger names to what the user inputs
+//-----------------it only works on th code above is commented out
 var submitGuess = document.querySelector("#submit-guess-button");
 var playerNameOne = document.querySelector("#player-name-one");
 var playerNameTwo = document.querySelector("#player-name-two");
@@ -11,35 +45,3 @@ function changeNameOne(event) {
  playerNameTwo.innerHTML = inputNameTwo.value;
  playerNameOne.innerHTML = inputNameOne.value;
 };
-
-// var numbers = [from 1, to 100];
-
-//     // random value generated 
-//     var y = Math.floor(Math.random() * 10 + 1); 
-      
-//     // counting the number of guesses 
-//     // made for correct Guess 
-//     var guess = 1; 
-      
-//     document.getElementById("submitguess").onclick = function(){ 
-      
-//     // number guessed by user      
-//     var x = document.getElementById("guessField").value; 
-  
-//    if(x == y) 
-//    {     
-//        console.log(winningPlayer)
-//                + guess + " GUESS "; 
-//    } 
-//    else if(x > y) /* if guessed number is greater 
-//                    than actual number*/ 
-//    {     
-//        guess++; 
-//        alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
-//    } 
-//    else
-//    { 
-//        guess++; 
-//        alert("OOPS SORRY!! TRY A GREATER NUMBER")
-//    } 
-// };
