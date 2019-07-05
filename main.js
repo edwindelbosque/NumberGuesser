07f03100054c579b0132dc54f32854634c9abf5c
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-07-04 20:27:31 -0600

    Add name changer function in JS

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
-var numbers = [from 1, to 100];
+var submitGuess = document.querySelector("#submit-guess-button");
+var playerNameOne = document.querySelector("#player-name-one");
+var playerNameTwo = document.querySelector("#player-name-two");
+var inputNameOne = document.querySelector("#input-name-one");
+var inputNameTwo = document.querySelector("#input-name-two");
 
-    // random value generated 
-    var y = Math.floor(Math.random() * 10 + 1); 
+submitGuess.addEventListener("click", changeNameOne);
+
+function changeNameOne(event) {
+ event.preventDefault();
+ playerNameTwo.innerHTML = inputNameTwo.value;
+ playerNameOne.innerHTML = inputNameOne.value;
+};
+
+// var numbers = [from 1, to 100];
+
+//     // random value generated 
+//     var y = Math.floor(Math.random() * 10 + 1); 
       
-    // counting the number of guesses 
-    // made for correct Guess 
-    var guess = 1; 
+//     // counting the number of guesses 
+//     // made for correct Guess 
+//     var guess = 1; 
       
-    document.getElementById("submitguess").onclick = function(){ 
+//     document.getElementById("submitguess").onclick = function(){ 
       
-    // number guessed by user      
-    var x = document.getElementById("guessField").value; 
+//     // number guessed by user      
+//     var x = document.getElementById("guessField").value; 
   
-   if(x == y) 
-   {     
-       console.log(winningPlayer)
-               + guess + " GUESS "; 
-   } 
-   else if(x > y) /* if guessed number is greater 
-                   than actual number*/ 
-   {     
-       guess++; 
-       alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
-   } 
-   else
-   { 
-       guess++; 
-       alert("OOPS SORRY!! TRY A GREATER NUMBER") 
-   } 
+//    if(x == y) 
+//    {     
+//        console.log(winningPlayer)
+//                + guess + " GUESS "; 
+//    } 
+//    else if(x > y) /* if guessed number is greater 
+//                    than actual number*/ 
+//    {     
+//        guess++; 
+//        alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
+//    } 
+//    else
+//    { 
+//        guess++; 
+//        alert("OOPS SORRY!! TRY A GREATER NUMBER")
+//    } 
+// };
