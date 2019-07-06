d70bdbde7ac4ecae5b8856cffad2855f92e7111b
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-07-06 12:36:29 -0600

    Add innerTexts to JS and HTML

--- main.js
+++ main.js
@@ -16,16 +16,25 @@ maxUser.innerText = maxRange.value;
 
 // This variables and function is to be able to change the challenger names to what the user inputs
 //-----------------it only works on th code above is commented out
-var submitGuess = document.querySelector("#submit-guess-button");
-var playerNameOne = document.querySelector("#player-name-one");
-var playerNameTwo = document.querySelector("#player-name-two");
+
+// ------------ Variables
 var inputNameOne = document.querySelector("#input-name-one");
 var inputNameTwo = document.querySelector("#input-name-two");
+var inputGuessOne = document.querySelector("#input-guess-one");
+var inputGuessTwo = document.querySelector("#input-guess-two");
+var submitGuessButton = document.querySelector("#submit-guess-button");
+var guessDisplayOne = document.querySelector("#guess-display-one")
+var guessDisplayTwo = document.querySelector("#guess-display-two")
+var playerNameOne = document.querySelector(".player-name-one");
+var playerNameTwo = document.querySelector(".player-name-two");
 
-submitGuess.addEventListener("click", changeNameOne);
+// --------------Event listeners
+submitGuessButton.addEventListener("click", displayInformation);
 
-function changeNameOne(event) {
- event.preventDefault();
+//  -------------Functions
+function displayInformation() {
  playerNameTwo.innerHTML = inputNameTwo.value;
  playerNameOne.innerHTML = inputNameOne.value;
+ guessDisplayOne.innerHTML = inputGuessOne.value;
+ guessDisplayTwo.innerHTML = inputGuessTwo.value;
 };
