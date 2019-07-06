7f11609abe98d8fcc82a09aa16892fe4413c6256
Jesse Maxim <jessmaxim303@gmail.com>
2019-07-06 11:43:26 -0600

    Add set range to JS

--- main.js
+++ main.js
@@ -1,30 +1,15 @@
-var numbers = [from 1, to 100];
+var minRange = document.getElementById("minRange");
+var maxRange = document.getElementById("maxRange");
+var rangeButton = document.querySelector('.updateRange');
+var minUser = document.querySelector('.minUser');
+var maxUser = document.querySelector('.maxUser');
 
-    // random value generated 
-    var y = Math.floor(Math.random() * 10 + 1); 
-      
-    // counting the number of guesses 
-    // made for correct Guess 
-    var guess = 1; 
-      
-    document.getElementById("submitguess").onclick = function(){ 
-      
-    // number guessed by user      
-    var x = document.getElementById("guessField").value; 
-  
-   if(x == y) 
-   {     
-       console.log(winningPlayer)
-               + guess + " GUESS "; 
-   } 
-   else if(x > y) /* if guessed number is greater 
-                   than actual number*/ 
-   {     
-       guess++; 
-       alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
-   } 
-   else
-   { 
-       guess++; 
-       alert("OOPS SORRY!! TRY A GREATER NUMBER") 
-   } 
+// create random number from the users min and max range
+
+rangeButton.addEventListener('click', updateRangeValue);
+
+function updateRangeValue(event){
+event.preventDefault();
+minUser.innerText = minRange.value;
+maxUser.innerText = maxRange.value;
+}
